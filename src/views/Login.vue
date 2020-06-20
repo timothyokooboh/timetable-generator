0f4f1eda7897d5b545cb10a055d4a3ed04fb0e56
timothyokooboh <okoobohtimothy@gmail.com>
2020-06-20 09:52:19 -0700

    Added sign up and login pages

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,3 +1,97 @@
 <template>
-    <h1> This is the login page</h1>
+    <div class="register">
+        <v-container>
+
+            <v-card>
+              
+                <v-card-title>
+                    <v-btn text x-large color="#7147B6"> Welcome Back</v-btn>
+                </v-card-title>
+
+                <v-form ref="form" @submit="submit" class="mx-5">
+                    <v-text-field
+                        v-model = "email"
+                        label = "Email Address"
+                        type = "email"
+                        append-icon = "mdi-email"
+                        outlined
+                        shaped 
+                        :rules = "emailRules" 
+                    >
+                    <!-- The "rules" prop is an array of functions used for validating users input -->
+
+                    </v-text-field>
+
+                    <v-text-field
+                        v-model = "password"
+                        label = "Password"
+                        type = "password"
+                        append-icon = "mdi-key"
+                        outlined
+                        shaped
+                        :rules = "passwordRules"
+                    >
+                    <!-- The "rules" prop is an array of functions used for validating users input -->
+                    </v-text-field>
+
+                    <v-btn type="submit"  :loading ="loadingContent" width = "100%" rounded color = "#6b37d8" class = "white--text mb-5">Log In</v-btn>
+
+                    <div class="flex-container pb-5">
+                        <div>
+                            <v-btn text small color="#7147B6"> Forgot Password? </v-btn>
+                        </div>
+                        <div>
+                            <v-btn text small color="#7147B6"> 
+                                <router-link to="/register"> Create Account </router-link>
+                            </v-btn>
+                        </div>
+                    </div>
+
+                </v-form>
+
+            </v-card>
+
+        </v-container>
+    </div>
 </template>
+
+<script>
+export default {
+    name: 'Home',
+    data() {
+        return {
+            email: "",
+            password: "",
+            loadingContent: false,
+            emailRules: [
+                v => !!v || "Email is required",
+                v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+            ],
+            passwordRules: [
+                v => !!v || "Password is required",
+                v => v.length >=8 || "Password must be at least 8 characters"
+            ]
+        }
+    },
+    methods: {
+       submit(e) {
+           if(this.$refs.form.validate()) {
+               this.loadingContent = true
+               return true
+           }
+           e.preventDefault()
+       }
+    }
+}
+</script>
+
+<style scoped>
+    .flex-container {
+        display: flex;
+        justify-content: space-between;
+    }
+
+   a {
+     color: #7147B6  
+   }
+</style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,3 +1,93 @@
 <template>
-    <h1>This is the registration page </h1>
+    <div class="register">
+        <v-container>
+            <v-card flat>
+              
+                <v-card-title>
+                    <v-btn text color="#7147B6" class="title"> CREATE ACCOUNT</v-btn>
+                </v-card-title>
+
+                <v-form ref="form" @submit="submit" class="mx-5 ">
+
+                <!-- I hope to include social login to enable users sign up with their gmail account -->
+
+                    <v-btn color="#6b37d8" class="white--text" rounded width="100%">
+                        <v-icon left>mdi-google</v-icon>
+                        <span> Sign UP With Google </span>
+                    </v-btn>
+
+                    <v-btn text color="#7147B6" class="my-4 title" left> OR</v-btn>
+
+                    <v-text-field
+                        v-model = "email"
+                        label = "Email Address"
+                        type = "email"
+                        append-icon = "mdi-email"
+                        outlined
+                        shaped
+                        autocomplete                       
+                        :rules = "emailRules"
+                    >
+                    <!-- The "rules" prop is an array of functions used for validating users input -->
+                    </v-text-field>
+
+                    <v-text-field
+                        v-model = "password"
+                        label = "Password"
+                        type = "password"
+                        append-icon = "mdi-key"
+                        outlined
+                        shaped
+                        :rules = "passwordRules"
+                    >
+                    <!-- The "rules" prop is an array of functions used for validating users input -->
+                    </v-text-field>
+
+                    <v-btn type="submit"  :loading ="loadingContent" width = "100%" rounded color = "#6b37d8" class = "white--text mb-5">Sign Up</v-btn>
+                    <!-- The "loading" prop is used to add a button loader -->
+
+                    <v-btn text small color="#7147B6" class="my-3">
+                        <router-link to="/login" > Have an account? Log In</router-link>
+                    </v-btn>
+                    
+                </v-form>
+            </v-card>
+        </v-container>
+    </div>
 </template>
+
+<script>
+export default {
+    name: 'Home',
+    data() {
+        return {
+            email: "",
+            password: "",
+            loadingContent: false,
+            emailRules: [
+                v => !!v || "Email is required",
+                v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+            ],
+            passwordRules: [
+                v => !!v || "Password is required",
+                v => v.length >=8 || "Password must be at least 8 characters"
+            ]
+        }
+    },
+    methods: {
+       submit(e) {
+           if(this.$refs.form.validate()) {
+               this.loadingContent = true
+               return true
+           }
+           e.preventDefault()
+       }
+    }
+}
+</script>
+
+<style scoped>
+   a {
+     color: #7147B6  
+   }
+</style>
